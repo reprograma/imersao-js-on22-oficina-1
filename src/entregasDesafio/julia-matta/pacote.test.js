c173988d1f080fbf3edb40a1a21eda8af001bb5d
julia <julhacolucci@gmail.com>
2022-08-14 17:21:07 -0300

    desafio finalizado

--- src/entregasDesafio/julia-matta/pacote.test.js
+++ src/entregasDesafio/julia-matta/pacote.test.js
@@ -1,12 +1,8 @@
 const { calcularPacote } = require("../../dominio/calculadora/Projeto/pacote");
-const {
-  MAX_HORAS_POR_PACOTE,
-  TAXAS_CONTRATUAIS_POR_PACOTE,
-} = require("../../dominio/calculadora/constantes/constantes");
 
 describe("teste com pacote básico", () => {
   test("retorna o pacote usado de acordo com as horas trabalhadas", () => {
-    const totalDeHorasPorProjeto = 32;
+    const totalDeHorasPorProjeto = 50;
     const result = calcularPacote(totalDeHorasPorProjeto);
     expect(result).toEqual("pacote_basico");
   });
@@ -14,18 +10,16 @@ describe("teste com pacote básico", () => {
 
 describe("teste com pacote intermediário", () => {
   test("retorna o pacote usado de acordo com as horas trabalhadas", () => {
-    const totalDeHorasPorProjeto = 32;
+    const totalDeHorasPorProjeto = 51;
     const result = calcularPacote(totalDeHorasPorProjeto);
-    expect(result).toEqual("pacote_basico");
+    expect(result).toEqual("pacote_intermediario");
   });
 });
 
 describe("teste com pacote premium", () => {
   test("retorna o pacote usado de acordo com as horas trabalhadas", () => {
-    const totalDeHorasPorProjeto = 32;
+    const totalDeHorasPorProjeto = 200;
     const result = calcularPacote(totalDeHorasPorProjeto);
-    expect(result).toEqual("pacote_basico");
+    expect(result).toEqual("pacote_premium");
   });
 });
-
-//DESAFIO: passar pro código o número de horas e receber como resultado o pacote que vai ser usado
