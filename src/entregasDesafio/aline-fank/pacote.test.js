e7dad541a48130f6f3aabae127e8f698e8f3a2d8
linefmv <alinemaciel018@gmail.com>
2022-08-14 20:43:19 -0300

    test: testing if hours are corresponding with projects

--- src/entregasDesafio/aline-fank/pacote.test.js
+++ src/entregasDesafio/aline-fank/pacote.test.js
@@ -1,10 +1,29 @@
 const { calcularPacote } = require('../../dominio/calculadora/Projeto/pacote');
 
 describe('Testar pacotes de acordo com as horas totais do projeto', () => {
-    test('testar o pacote básico', () => {
-        const horas = 50;
-        const result = calcularPacote(horas);
+    test('verificar se as horas realizadas correspondem ao pacote básico', () => {
+        const horasRealizadas = 50;
+        const result = calcularPacote(horasRealizadas);
         expect(result).toEqual('pacote_basico');
+    })
+
+    test('verificar se as realizadas horas não correspondem ao pacote básico', () => {
+        const horasRealizadas = 60;
+        const result = calcularPacote(horasRealizadas);
+        expect(result).not.toEqual('pacote_basico');
+    })
+
+    test('verificar se as horas realizadas correspondem ao pacote intermediário', () => {
+        const horasRealizadas = 89;
+        console.log(horasRealizadas)
+        const result = calcularPacote(horasRealizadas);
+        expect(result).toEqual('pacote_intermediario');
+    })
+    
+    test('verificar se as horas realizadas correspondem ao pacote premium', () => {
+        const horasRealizadas = 140;
+        const result = calcularPacote(horasRealizadas);
+        expect(result).toEqual('pacote_premium');
     })
 })
 
