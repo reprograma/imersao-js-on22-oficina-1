4536e29a925cfad8b1ff8e900bd9bad82aa45663
linefmv <alinemaciel018@gmail.com>
2022-08-14 14:18:14 -0300

    test: if the value rounded

--- src/entregasDesafio/aline-fank/valorHora.test.js
+++ src/entregasDesafio/aline-fank/valorHora.test.js
@@ -1,1 +1,11 @@
-// Testes aqui
+const { calcularValorPorHora } = require('../../dominio/calculadora/Hora/valorHora');
+
+describe('Valor por hora', () => {
+    test('retorna o valor arrendado correto dado o valor recebido no mês', () => {
+        const valorRecebidoNoMes = 5000;
+
+        const result = calcularValorPorHora(valorRecebidoNoMes)
+
+        expect(result).toEqual(29);
+    })
+});
