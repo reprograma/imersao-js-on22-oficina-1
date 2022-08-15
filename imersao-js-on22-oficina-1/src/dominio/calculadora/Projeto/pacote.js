07d9c83b11c688bd6b697087d6d7fc31d268969b
Fábia Emanuele Cardoso Sena <61851602+fabiacardoso@users.noreply.github.com>
2022-08-15 17:00:55 -0300

    Entrega

---
+++ imersao-js-on22-oficina-1/src/dominio/calculadora/Projeto/pacote.js
@@ -0,0 +1,15 @@
+const { MAX_HORAS_POR_PACOTE } = require('../constantes/constantes');
+
+const calcularPacote = (totalDeHorasPorProjeto) => Object.entries(MAX_HORAS_POR_PACOTE)
+  .find(([key, value]) => value >= totalDeHorasPorProjeto
+)[0];
+
+exports.calcularPacote = calcularPacote;
+
+/* Na linha 4, o código que recebemos na oficina dizia que "value" deveria ser maior que "totalDeHorasPorProjeto". Com o código assim o nosso teste 
+   do pacote não deu certo. Então nós pensamos em simplismente diminuir o valor de 50 no teste para 49 e ai sim ele funcionou. Foi então que eu
+   percebi que poderia ser melhor mudar algo no código original e não adequar o valor no teste.     
+   
+   Revisando o código eu pude perceber que seria possível resolver isso colocando o "value" como maior ou igual a "totalDeHorasPorProjeto". 
+   Dessa forma o teste passou perfeitamente!!!
+*/
